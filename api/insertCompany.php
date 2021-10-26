8dfce14ebaca490ac9e3739e316f9b2d69fffc8e
Sofia Toro Chambi <sofie.torch@outlook.com>
2021-10-26 12:10:18 -0400

    insertCompany file uploaded for testing

---
+++ api/insertCompany.php
@@ -0,0 +1,25 @@
+<?php
+require "config.php";
+$pdo = DBConnection::GetConnection();
+/*$name = isset($_POST['name']) ? $_POST['name'] : "";
+$nit = isset($_POST['nit']) ? $_POST['nit'] : "";*/
+
+$name = $_POST['name'];
+$nit = $_POST['nit'];
+try {
+    $pdo->beginTransaction();
+    $insertSql = "
+    INSERT INTO `company`(`name`,`state`, `NIT`) 
+    VALUES (:name, 1, :nit)
+  ";
+    $statement = $pdo->prepare($insertSql);
+    $statement->execute(array(
+        "name" => $name,
+        "nit" => $nit,
+    ));
+    $pdo->commit();
+    echo json_encode("Success");
+} catch (\Exception $e) {
+    $db->rollback();
+    echo json_encode($e);
+}
